87dc972a25c31db07c664afe42590805b22c03bb
Victor <victor_3vlez@hotmail.com>
2024-09-20 21:12:46 +0200

    Modified simulator size and physics

--- CODE/Mini_gravity_simulator_javascript/gravity.js
+++ CODE/Mini_gravity_simulator_javascript/gravity.js
@@ -1,106 +1,144 @@
-//Get the canvas to draw on
-canvas = document.getElementById("field").getContext("2d");
-
-//Draw each particle
-draw = (x,y,c,s)=>{
-    canvas.fillStyle=c
-    canvas.fillRect(x,y,s,s);
-}
-
-//Create a single particle and return it
-particle=(x,y,z,c,m,r)=>{
-    let vx = (Math.random()*2-1) * 10;
-    let vy = (Math.random()*2-1) * 10;
-    let vz = (Math.random()*2-1) * 10;
-    return{"x":x,"y":y,"z":z,"vx":vx,"vy":vy,"vz":vz,"color":c,"mass":m,"radius":r};
-}
-
-//Get a random number between 0 and 240000  for x
-randomX=()=>{
-    return Math.random()*240000;
-}
-
-//Get a random number between 0 and 240000  for y
-randomY=()=>{
-    return Math.random()*240000;
-}
-
-//Create a group of particles and add it to the main group
-create=(number,color,mass,radius)=>{
-
-    for(let i=0; i<number; i++){
-        particles.push(particle(randomX(),randomY(),randomX(),color,mass,radius));
+    //Draw a filled rectangle
+    //X and y are the origin, c is the color, w and h are the size (width and height from origin)
+    draw = (x,y,c,w,h)=>{
+        context.fillStyle = c
+        context.fillRect(x,y,w,h);
     }
-    return particles;
-}
+
+    //Draw a filled circle
+    //X and y are the origin, c is the color, r is the radius
+    draw_circle = (x,y,c,r)=>{
+        context.fillStyle = c
+        context.beginPath();
+        context.arc(x, y, r,0 , 2 * Math.PI);
+        context.fill();
+    }
+
+    //Create a single particle and return it
+    particle = (x,y,c,m,r)=>{
+
+        //random velocities
+        let vx = (Math.random()*2-1) * 5;
+        let vy = (Math.random()*2-1) * 5;
+
+        return{"x":x,"y":y,"vx":vx,"vy":vy,"color":c,"mass":m,"radius":r};
+    }
+
+    //Get a random number between 0 and 240000  for x
+    randomX=()=>{
+        return Math.random() * WIDTH;
+    }
+
+    //Get a random number between 0 and 240000  for y
+    randomY=()=>{
+        return Math.random() * HEIGHT;
+    }
+
+    //Create a group of particles and add it to the main group
+    create=(number,color,mass,radius)=>{
+
+        for(let i=0; i<number; i++){
+            //Create a particle in a random location
+            particles.push(particle(randomX(),randomY(),color,mass,radius));
+        }
+        return particles;
+    }
 
 
-//gravity function for each particle
-gravity = (g)=>{
+    //gravity function for each particle
+    gravity = ()=>{
+
+        
 
         for(let i = 0 ; i < particles.length; i++){
 
             let fx = 0;
             let fy = 0;
-            let fz = 0;
             let a = particles[i];
             for(let j = 0 ; j < particles.length; j++){
 
-                if (i == j) continue;
                 let b = particles[j];
 
                 let dx = b.x -a.x;
                 let dy = b.y -a.y;
-                let dz = b.z -a.z;
-                let d = Math.sqrt(dx*dx + dy*dy + dz*dz);
+                let d = Math.sqrt(dx*dx + dy*dy)+0.1;
 
-                if(d > (a.radius + b.radius) *400){
-                    let F = g* (a.mass * b.mass) / (d*d*d) / a.mass;
-                    fx += F * dx;
-                    fy += F * dy;
-                    fz += F * dz;
+                let dis;
+                let g;
+
+                if(d < 25){
+
+                    g = 0.000001;
+                    dis = d*d;
+
                 }
+                else if(d < 50){
+                    g = 0.1;
+                    dis = d*d*d;
+                }
+                else{
+                    g = 50;
+                    dis = d*d*d*d;
+                }
+                
+                //To simplify calculations g is 1, so it is omitted in  g * (a.mass * b.mass) / (d*d) / a.mass;
+                //also, the mass of 'a' cancels out in the equation.
+                let F = g * (b.mass / dis);
+                fx += F * dx;
+                fy += F * dy;
+                
             }
 
-            a.vx += fx;
-            a.vy += fy;
-            a.vz += fz;
+            //add the force and subtract a small part of speed
+            a.vx = (a.vx + fx) * 0.992;
+            a.vy = (a.vy + fy) * 0.992;
 
-            a.vx *= 0.992;
-            a.vy *= 0.992;
-            a.vz *= 0.992;
-
+            //Update position
             a.x += a.vx;
             a.y += a.vy;
-            a.z += a.vz;
         }
     }
 
-//Update function to draw everything
-update=()=>{
-    gravity(10000);
-    canvas.clearRect(0,0,600,600);
-    draw(0,0,"black",600);
-    for(i=0; i<particles.length; i++){
-        draw(particles[i].x/400,particles[i].y/400,particles[i].color,0.8);
+
+
+    //Update function to draw everything
+    update=()=>{
+
+        //Clear the area before drawing particles
+        draw(0,0,"black",WIDTH,HEIGHT);
+        
+        gravity();
+        
+        for(i=0; i<particles.length; i++){
+
+            let p = particles[i];
+            draw_circle(p.x , p.y, p.color, p.radius);
+        }
+        requestAnimationFrame(update);
     }
-    requestAnimationFrame(update);
-}
 
 
-//Array to store all the particles of the simulator
-particles=[];
+    //Get the canvas to draw on
+    canvas = document.getElementById("field");
+    context = canvas.getContext("2d");
 
-//Create all the particles
-blue = create(1000,"blue",100,3);
-yellow = create(1000,"yellow",100,3);
-green = create(1000,"green",100,3);
-purple = create(1000,"purple",100,3);
-red = create(1000,"red",100,3);
-white = create(1000,"white",100,3);
-//Ten black holes?
-black = create(10,"black",200000,10);
+    //Width and height of map
+    const WIDTH = 1400;
+    const HEIGHT = 700;
 
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
 
-//Start the simulator
-update();
+    //Array to store all the particles of the simulator
+    particles=[];
+
+    //Create all the particles
+    red = create(700,"red",1,1.5);
+    red = create(700,"#6bf",2,1.5);
+    yellow = create(700,"yellow",3,1.5);
+    white = create(700,"white",4,1.5);
+    giant_blue = create(400,"#9df",10,1.5);
+    //black_holes = create(5,"#102",1000000,10);
+
+    //Start the simulator
+    update();
